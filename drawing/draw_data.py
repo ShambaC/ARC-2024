cf35738b0e8c8f9563a2c57910eb5d3b078358d6
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-07-20 19:48:52 +0530

    add grid to the images

--- drawing/draw_data.py
+++ drawing/draw_data.py
@@ -15,6 +15,23 @@ colorList = [[229,58,163],
              [153,153,153],
              [0,0,0]]
 
+def draw_grid(img, grid_shape, color=(0, 0, 0), thickness=1):
+    h, w, _ = img.shape
+    rows, cols = grid_shape
+    dy, dx = h / rows, w / cols
+
+    # draw vertical lines
+    for x in np.linspace(start=dx, stop=w-dx, num=cols-1):
+        x = int(round(x))
+        cv2.line(img, (x, 0), (x, h), color=color, thickness=thickness)
+
+    # draw horizontal lines
+    for y in np.linspace(start=dy, stop=h-dy, num=rows-1):
+        y = int(round(y))
+        cv2.line(img, (0, y), (w, y), color=color, thickness=thickness)
+
+    return img
+
 # window = tkinter.Tk()
 
 # window.title = "Visualise"
@@ -61,6 +78,10 @@ for key in trainingData :
                 outputImg[i:i+pixelSize, j:j+pixelSize] = colorList[outputMat[p][q]]
                 q += 1
             p += 1
+
+        # Add grid to the image
+        inputImg = draw_grid(inputImg, (len(inputMat), len(inputMat[0])))
+        outputImg = draw_grid(outputImg, (len(outputMat), len(outputMat[0])))
 
         while True :
 
